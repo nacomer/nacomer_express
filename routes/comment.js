804b7b025c9e43f7682fafc6547bd3716d7285f6
t-isano <t-isano@nri.co.jp>
2020-11-24 17:18:48 +0900

    refactored source, deleting comment
    Co-authored-by: t-isano <t-isano@users.noreply.github.com>
    Co-authored-by: Ryoma-Aoki <ryoma-aoki@users.noreply.github.com>

--- routes/comment.js
+++ routes/comment.js
@@ -2,14 +2,6 @@ const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/comment");
 
-// router.get("/", apiController.getAllHobbies);
-
-// router.get("/:id", apiController.getHobby);
-
-// router.get("/:id/comment", commentController.getAllComments);
-
-// router.post("/:id/comment", commentController.postComment);
-
 router.put("/:id", commentController.putComment);
 
 router.delete("/:id", commentController.deleteComment);
