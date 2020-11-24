1deddd16ff5f771407a2858924f8f4f5ccd6ed08
t-isano <t-isano@nri.co.jp>
2020-11-24 18:21:59 +0900

    modifiedLogin

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,25 @@
 const usersService = require("../services/users");
+const jwt = require("jsonwebtoken");
+const config = require("../config/jwt.config");
 
 exports.getUser = async function (req, res) {
   try {
     const user = await usersService.getUser(req.body.name, req.body.password);
-    if (user !== undefined) {
-      user["Auth"] = "true";
+    if (user !== null) {
+      const payload = {
+        userId: user.id,
+      };
+      const token = jwt.sign(payload, config.jwt.secret, config.jwt.options);
+      return res.status(200).json({
+        isSuccess: true,
+        token: token,
+      });
+    } else {
+      return res.status(404).json({
+        isSuccess: false,
+        message: "ユーザーIDまたはパスワードが違います。",
+      });
     }
-    return res.status(200).json(user);
   } catch (e) {
     return res.status(400).json({ status: 400, message: e.message });
   }
