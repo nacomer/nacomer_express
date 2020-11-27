def952cc5ee35e0e286fe6dbe085f946e8e83293
TomoyaHirano <tomoya.hirano.th@gmail.com>
2020-11-27 15:35:06 +0900

    add seed, migrations, models

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "postgres",
-    password: "password",
-    database: "nacomer",
-    host: "localhost",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
     loglevel: "debug",
   },
