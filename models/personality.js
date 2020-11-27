7a1c65259814438d024076dcf3d98791ba1ca2d1
h-tsuboi918 <70654579+h-tsuboi918@users.noreply.github.com>
2020-11-27 16:13:05 +0900

    [fix] lint error

--- models/personality.js
+++ models/personality.js
@@ -9,12 +9,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    // static associate(models) {
       // Personality.hasMany(models.Hobby, {
       //   foreignKey: "personalityId",
       //   as: "Hobbies",
       // });
-    }
+    // }
   };
   Personality.init({
     name: DataTypes.STRING
